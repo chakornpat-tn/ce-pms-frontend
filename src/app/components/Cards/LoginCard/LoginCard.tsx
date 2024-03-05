4fa3b54394decef0aead1ceb2a80ea025baadb53
chakornpat <chakornpat0206@outlook.com>
2024-03-06 01:24:08 +0700

    :memo:

--- src/app/components/Cards/LoginCard/LoginCard.tsx
+++ src/app/components/Cards/LoginCard/LoginCard.tsx
@@ -1,18 +1,39 @@
 import { ButtonWithArrow } from '@/app/components/Buttons'
+import Image from 'next/image'
 
 type Props = {
   title: string
+  imageName: string
+  pathImage?: string
+  bgColor: string
+  urlPath: string
 }
 
-const LoginCard = ({ title }: Props) => {
+const LoginCard = ({
+  title,
+  imageName,
+  pathImage,
+  bgColor,
+  urlPath,
+}: Props) => {
   return (
-    <div className="h-5 min-w-[180px] md:w-[380px] min-h-[180px] m-5 bg-white border-[2px] border-primary2-500  rounded-lg shadow-lg">
-      <div className="flex flex-col justify-around items-center h-full">
+    <div className="h-auto  md:min-h-[280px] w-auto md:min-w-[320px] m-5  bg-white  rounded-lg shadow-2xl">
+      <div className="flex flex-col justify-around items-center h-full py-2">
+        {!!pathImage && (
+          <div className={`rounded-full ${bgColor} w-auto h-auto`}>
+            <Image
+              src={`/Images/${pathImage}.png`}
+              alt={imageName}
+              width="124"
+              height="124"
+            />
+          </div>
+        )}
         <h1 className="text-2xl font-bold">{title}</h1>
-        <ButtonWithArrow Title="เข้าสู่ระบบ" />
+        <ButtonWithArrow Title="เข้าสู่ระบบ" path={urlPath} />
       </div>
     </div>
   )
 }
 
-export default LoginCard
+export { LoginCard }
