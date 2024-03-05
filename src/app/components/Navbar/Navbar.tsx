9a7399c9d13e969892de50b7d1c42b06ce41604a
Guitarrr <guitar3025@gmail.com>
2024-03-05 18:02:18 +0700

    Navbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -10,28 +10,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
   return (
-    <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="flex justify-between items-center w-full top-0 border-b px-20 py-3 border-gray-200 dark:border-gray-300">
+      {/* <div className="flex justify-between mx-left-20 p-4 bg-red-500"> */}
         <a href='/#' className="flex items-center space-x-3 rtl:space-x-reverse">
-        <span className="self-center md:text-xl font-semibold whitespace-nowrap dark:text-primary1">Project Management System</span>
+        <span className="md:text-xl font-semibold whitespace-nowrap dark:text-primary1">Project Management System</span>
         </a>
+      <div className="flex items-center rtl:space-x-reverse ml-auto">
+      <span className='md:text-sm'>
+      <Button className='text-primary2-400 bg-primary2-100'>Login/เข้าสู่ระบบ</Button>
+      </span>
       </div>
-      <div className="flex items-center space-x-6 rtl:space-x-reverse">
-      <Button className='text-primary2-400 bg-primary2-100 right-5'>Login/เข้าสู่ระบบ</Button> 
-        </div>
+      {/* </div> */}
     </nav>
-
-    // <Box sx={{ flexGrow: 1 ,display: 'flex', justifyContent: 'flex-end'}}>
-    //   <AppBar position="static">
-    //     <Toolbar>
-    //     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-    //       <div className="hidden lg:flex lg:gap-x-12">
-    //         <a href="/#" className="text-3xl leading-6 text-primary2-100 left-5">Project Management System</a>
-    //       </div>
-    //     </Typography>
-    //     <Button className='text-primary2-400 bg-primary2-100 right-5'>Login/เข้าสู่ระบบ</Button>     
-    //     </Toolbar>
-    //   </AppBar>
-    // </Box>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import Navbar from './components/Navbar'
 export default function Home() {
   return (
     <div>
-    <div className="flex items-center justify-center">
+    <div>
       <Navbar/>
     </div>
-    {/* <div className='mx-auto max-2xl text-center my-10'>
+
+    <div className='mx-auto max-2xl text-center my-5'>
       <h2 className='text-3xl font-bold tracking-tight text-black sm:text-4xl'>โครงงานวิศวกรรมไฟฟ้า สาขาวิศวกรรมคอมพิวเตอร์</h2>
-    </div> */}
+    </div>
     </div>
   )
 }
