4b3c8f54966a6a4aa56dec14d78d7c88a1d6d6a4
chakornpat <chakornpat0206@outlook.com>
2024-03-28 21:58:53 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { AllpjCard, ProjCard } from './components/Cards/ProjCard/ProjCard'
-import Navbar from './components/Navbar'
+import { AllpjCard, ProjCard } from "./components/Cards/ProjCard/ProjCard";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-bg_primary min-h-screen">
-      <Navbar />
-        <div className="mx-auto max-2xl text-center my-5">
+      <div className="min-h-screen bg-bg_primary">
+        <Navbar />
+        <div className="max-2xl mx-auto my-5 text-center">
           <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
             โครงงานวิศวกรรมไฟฟ้า สาขาวิศวกรรมคอมพิวเตอร์
           </h2>
         </div>
         <div className="flex w-auto justify-center">
           <div className="px-4 py-2">
-            <h5 className="text-2xl text-gray-900 mb-4">
+            <h5 className="mb-4 text-2xl text-gray-900">
               เลือกปีการศึกษาที่ต้องการตรวจสอบ
             </h5>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-3 h-full  ">
+            <div className="grid h-full grid-cols-1 gap-4 md:grid-cols-3  ">
               <ProjCard path="/2555" />
               <ProjCard path="#" />
               <ProjCard path="#" />
@@ -28,5 +28,5 @@ export default function Home() {
         </div>
       </div>
     </>
-  )
+  );
 }
