976065c3d4da8fedb77846ac4f50ae271e229e83
chakornpat <chakornpat0206@outlook.com>
2024-12-03 03:40:23 +0700

    edit middleware

--- src/app/teacher/status/layout.tsx
+++ src/app/teacher/status/layout.tsx
@@ -1,38 +1,59 @@
+
 'use client'
-import React, { useState } from 'react'
+import userRoles from '@/constants/userRoles/userRoles'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useState, useEffect } from 'react'
 
 type Props = {
   children: React.ReactNode
 }
-function layout({ children }: Props) {
+
+function Layout({ children }: Props) {
   const pathname = usePathname()
+  const [role, setRole] = useState(userRoles.preProjectTeacher)
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      const res = await fetch('/api/auth/check')
+      const data = await res.json()
+      if (data.user) {
+        setRole(Number(data.user.role))
+      }
+    }
+    fetchRole()
+  }, [])
+
   return (
     <>
       <div className="mb-2 flex flex-col items-start justify-between md:flex-row md:items-center">
         <h1 className="text-2xl font-bold text-primary1 md:text-4xl">
-          โครงงานที่เป็นที่ปรึกษา
+          สถานะโครงงาน
         </h1>
       </div>
       <div className="my-[15px] w-full rounded-md bg-white shadow-sm">
         <ul className="-mb-px flex flex-wrap">
-          <li className="me-2">
-            <Link
-              href="/teacher/status"
-              className={`primary-hover inline-block rounded-t-lg border-b-2 p-4 text-primary1 ${pathname === '/teacher/status' ? 'border-primary1' : 'border-transparent'}`}
-            >
-              เตรียมโครงงาน
-            </Link>
-          </li>
-          <li className="me-2">
-            <Link
-              href="/teacher/status/project"
-              className={`inline-block rounded-t-lg border-b-2 p-4 text-primary1 ${pathname === '/teacher/status/project' ? 'border-primary1' : 'border-transparent'}`}
-            >
-              โครงงาน
-            </Link>
-          </li>
+          {role === userRoles.ProjectTeacher && (
+            <li className="me-2">
+              <Link
+                href="/teacher/status/project"
+                className={`inline-block rounded-t-lg border-b-2 p-4 text-primary1 ${pathname === '/teacher/status/project' ? 'border-primary1' : 'border-transparent'}`}
+              >
+                โครงงาน
+              </Link>
+            </li>
+          )}
+          {(role === userRoles.ProjectTeacher ||
+            role === userRoles.preProjectTeacher) && (
+            <li className="me-2">
+              <Link
+                href="/teacher/status"
+                className={`primary-hover inline-block rounded-t-lg border-b-2 p-4 text-primary1 ${pathname === '/teacher/status' ? 'border-primary1' : 'border-transparent'}`}
+              >
+                เตรียมโครงงาน
+              </Link>
+            </li>
+          )}
         </ul>
         <div className="flex flex-row items-center justify-center p-4">
           {children}
@@ -42,4 +63,4 @@ function layout({ children }: Props) {
   )
 }
 
-export default layout
+export default Layout
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,48 +4,47 @@ import config2 from './config'
 import userRoles from './constants/userRoles/userRoles'
 
 export async function middleware(request: NextRequest) {
+  const TeacherRole = [
+    userRoles.Teacher,
+    userRoles.preProjectTeacher,
+    userRoles.ProjectTeacher,
+  ]
+
   try {
     const token = request.cookies.get('token')
 
     if (!token?.value) {
-      if (request.nextUrl.pathname.startsWith('/login')) {
-        return NextResponse.next()
-      } else {
-        return NextResponse.redirect(new URL('/login', request.url))
-      }
+      return handleNoToken(request)
     }
 
     const secret = new TextEncoder().encode(config2.TOKEN_SECRET)
     const { payload } = await jwtVerify(token.value, secret)
-    if (payload.exp && payload.exp * 1000 < Date.now()) {
-      const response = NextResponse.redirect(new URL('/login', request.url))
-      response.cookies.delete('token')
-      return response
+
+    if (isTokenExpired(payload)) {
+      return handleExpiredToken(request)
     }
-    
+
     const pathname = request.nextUrl.pathname
 
-    const TeacherRole = [
-      userRoles.Teacher,
-      userRoles.preProjectTeacher,
-      userRoles.ProjectTeacher,
-    ]
-
-    if (
-      pathname.startsWith('/teacher') &&
-      !TeacherRole.includes(payload.role as number)
-    ) {
-      return NextResponse.redirect(new URL('/', request.url))
+    if (isUnauthorizedTeacherRole(payload, pathname)) {
+      return NextResponse.redirect(new URL('/login', request.url))
     }
 
-    if (
-      pathname.startsWith('/project') &&
-      (payload.role as number) !== userRoles.Student
-    ) {
-      return NextResponse.redirect(new URL('/', request.url))
+    if (isUnauthorizedProjectAccess(payload, pathname)) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    if (TeacherAccess(payload, pathname)) {
+      if (payload.role === userRoles.Student)
+        NextResponse.redirect(new URL('/project', request.url))
+      return NextResponse.redirect(new URL('/teacher', request.url))
     }
 
     if (pathname.startsWith('/login')) {
+      if (payload.role === userRoles.Student)
+        NextResponse.redirect(new URL('/project', request.url))
+      if (TeacherRole.includes(payload.role as number))
+        NextResponse.redirect(new URL('/teacher', request.url))
       return NextResponse.redirect(new URL('/', request.url))
     }
 
@@ -54,6 +53,53 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 }
+
+function handleNoToken(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/login')) {
+    return NextResponse.next()
+  }
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+
+function isTokenExpired(payload: any): boolean {
+  return payload.exp && payload.exp * 1000 < Date.now()
+}
+
+function handleExpiredToken(request: NextRequest) {
+  const response = NextResponse.redirect(new URL('/login', request.url))
+  response.cookies.delete('token')
+  return response
+}
+
+function TeacherAccess(payload: any, pathname: string): boolean {
+  return (
+    payload.role === userRoles.Teacher &&
+    ['/teacher/users', '/teacher/docs', '/teacher/status'].some(
+      restrictedPath => pathname.startsWith(restrictedPath),
+    )
+  )
+}
+
+function isUnauthorizedTeacherRole(payload: any, pathname: string): boolean {
+  const TeacherRole = [
+    userRoles.Teacher,
+    userRoles.preProjectTeacher,
+    userRoles.ProjectTeacher,
+  ]
+
+  return (
+    pathname.startsWith('/teacher') &&
+    !TeacherRole.includes(payload.role as number)
+  )
+}
+
+function isUnauthorizedProjectAccess(payload: any, pathname: string): boolean {
+  return (
+    pathname.startsWith('/project') &&
+    (payload.role as number) !== userRoles.Student
+  )
+}
+
 export const config = {
   matcher: ['/teacher/:path*', '/project/:path*', '/login/:path*'],
 }
