596ab253119c9be55a02644c153f38342c7db35b
Guitarrr <guitar3025@gmail.com>
2025-03-10 11:38:47 +0700

    change color project, prepro for teachers

--- src/app/teacher/consultant/project/page.tsx
+++ src/app/teacher/consultant/project/page.tsx
@@ -95,7 +95,7 @@ function Page({}: Props) {
     }
 
     return (
-      <section className="relative mt-4 overflow-x-auto bg-gradient-to-b from-orange-400 to-primary2-200 p-4 shadow-md sm:rounded-md">
+      <section className="relative mt-4 overflow-x-auto bg-gradient-to-b from-orange-400 to-orange-200 p-4 shadow-md sm:rounded-md">
         <article>
           <div className="mb-4 flex flex-col items-start justify-between md:flex-row md:items-center">
             <h2 className="mb-4 text-xl font-bold md:mb-0 md:text-2xl">
